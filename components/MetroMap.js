c9fd283d28b6f340d3b9696c24e5987852d12cb2
Axel Clark <axelclark2@yahoo.com>
2018-05-27 20:18:50 -0500

    Add StatusBar and make text dark
    * Add StatusBar component to make text dark
    * Use networkActivityIndicatorVisible when fetching
    * Closes #19
    
    TODO fix WebView issue in MetroMap

--- components/MetroMap.js
+++ components/MetroMap.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import {
+  StatusBar,
+  View,
   WebView
 } from 'react-native';
+import { BAR_STYLE } from '../constants/styles'
 
 class MetroMap extends React.Component {
+  state = {
+    loading: true
+  }
+
   static navigationOptions = {
     drawerLabel: 'Metro Map'
   };
 
   render() {
+    const { loading } = this.state
     return (
-      <WebView
-        source={{uri: 'https://www.wmata.com/schedules/maps/upload/2017-System-Map.pdf'}}
-      />
+      <View style={{ flex: 1 }}>
+        <StatusBar
+          style={{ flex: 1 }}
+          barStyle={BAR_STYLE}
+          networkActivityIndicatorVisible={loading}
+        />
+        <WebView
+          source={{uri: 'https://www.wmata.com/schedules/maps/upload/2017-System-Map.pdf'}}
+          onLoadEnd={() => this.setState({ loading: false })}
+        />
+      </View>
     );
   }
 }
